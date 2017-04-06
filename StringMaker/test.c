2b3b7ea0f32fdb109dd77df55e818cd0f8d51634
M.K <m.k@MKdeMacBook-Pro.local>
2017-04-06 23:09:12 +0800

    sjp

---
+++ StringMaker/test.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int test1()
+{
+	return 0;
+}
+
+int test2()
+{
+	return 0;
+}
+
+int test3()
+{
+	return 0;
+}
+
+int test4()
+{
+	return 0;
+}
+
+int test5()
+{
+	return 0;
+}
+
+int test6()
+{
+	return 0;
+}
+
+int test7()
+{
+	return 0;
+}
+
+int test8()
+{
+	return 0;
+}
+
+int test9()
+{
+	return 0;
+}
+
+int main()
+{
+	return 0;
+}
