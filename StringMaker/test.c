e8fd5826708d5ecf3c94ef3603abd89bc58a1ee5
M.K <m.k@MKdeMacBook-Pro.local>
2017-04-06 23:42:31 +0800

    sjp

--- StringMaker/test.c
+++ StringMaker/test.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include "StringMaker.h"
 
 int test1()
 {
@@ -50,5 +50,14 @@ int test9()
 
 int main()
 {
+	if(test1()) printf("test1 successed\n");
+	if(test2()) printf("test2 successed\n");
+	if(test3()) printf("test3 successed\n");
+	if(test4()) printf("test4 successed\n");
+	if(test5()) printf("test5 successed\n");
+	if(test6()) printf("test6 successed\n");
+	if(test7()) printf("test7 successed\n");
+	if(test8()) printf("test8 successed\n");
+	if(test9()) printf("test9 successed\n");
 	return 0;
 }
