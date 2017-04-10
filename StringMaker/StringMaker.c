06e8cb11837fa10dc9af5caf526fb8de71e22d11
M.K <m.k@MKdeMacBook-Pro.local>
2017-04-10 22:47:46 +0800

    sjp

--- StringMaker/StringMaker.c
+++ StringMaker/StringMaker.c
@@ -246,29 +246,123 @@ int DownLine2HungaryX(const char *src, char **dst)
 	return 0;
 }
 
-// //下划线型转驼峰型
-// char *DownLine2Camelback(const char *str)
-// {
+//下划线型转驼峰型
+char *DownLine2Camelback(const char *src)
+{
+	// char * hungary_test = "aaa_bb_ccc_ddd";
+	if(src == NULL)
+	{
+		printf("src == NULL\n");
+		return NULL;
+	}
+	int index = 0;
+	int length = strlen(src);
+	char *dst = (char *)malloc(length * 2);
+	memset(dst, 0, length * 2);
 
-// }
+	for(int i=0; i < length; i++)
+	{
+		char c = src[i];
+		if(c == '_')
+		{
+			i ++;
+			c = src[i] - 32;
+		}
+		dst[index] = c;
+		index ++;
+	}
+	dst[index] = '\0';
+	return dst;
+}
 
-// //下划线型转驼峰增强型
-// int DownLine2CamelbackX(const char *src, char **dst)
-// {
+//下划线型转驼峰增强型
+int DownLine2CamelbackX(const char *src, char **dst)
+{
+	// char * hungary_test = "aaa_bbCccDdd";
+	if(src == NULL || dst == NULL)
+	{
+		printf("src == NULL || dst == NULL\n");
+		return -1;
+	}
+	int index = 0;
+	int length = strlen(src);
+	char *dstx = *dst;
+	for(int i=0; i < length; i++)
+	{
+		char c = src[i];
+		if(c == '_')
+		{
+			i ++;
+			c = src[i] - 32;
+		}
+		dstx[index] = c;
+		index ++;
+	}
+	dstx[index] = '\0';
+	return 0;
+}
 
-// }
+//下划线型转帕斯卡型
+char *DownLine2Pascal(const char *src)
+{
+	// char * hungary_test = "aaa_bb_ccc_ddd";
+	if(src == NULL)
+	{
+		printf("src == NULL\n");
+		return NULL;
+	}
+	int index = 0;
+	int length = strlen(src);
+	char *dst = (char *)malloc(length * 2);
+	memset(dst, 0, length * 2);
 
-// //下划线型转帕斯卡型
-// char *DownLine2Pascal(const char *str)
-// {
+	dst[index] = src[index] - 32;
+	index ++;
+	for(int i = 1; i < length; i++)
+	{
+		char c = src[i];
+		if(c == '_')
+		{
+			i ++;
+			c = src[i] - 32;
+		}
+		dst[index] = c;
+		index ++;
+	}
+	dst[index] = '\0';
+	return dst;
+}
 
-// }
+//下划线型转帕斯卡增强型
+int DownLine2PascalX(const char *src, char **dst)
+{
+	// char * hungary_test = "aaa_bbCccDdd";
+	if(src == NULL || dst == NULL)
+	{
+		printf("src == NULL || dst == NULL\n");
+		return -1;
+	}
+	int index = 0;
+	int length = strlen(src);
+	char *dstx = *dst;
 
-// //下划线型转帕斯卡增强型
-// int DownLine2PascalX(const char *src, char **dst)
-// {
+	dstx[index] = src[index] - 32;
+	index ++;
 
-// }
+	for(int i = 1; i < length; i++)
+	{
+		char c = src[i];
+		if(c == '_')
+		{
+			i ++;
+			c = src[i] - 32;
+		}
+		dstx[index] = c;
+		index ++;
+	}
+	dstx[index] = '\0';
+	return 0;
+}
 
 // //驼峰型转匈牙利型
 // char *Camelback2Hungary(const char *str)
--- StringMaker/test.c
+++ StringMaker/test.c
@@ -84,7 +84,6 @@ int test5()
 		return 1;
 	}	
 	else	return 0;
-	return 0;
 }
 
 int test6()
@@ -118,19 +117,17 @@ int test7()
 		return 1;
 	}	
 	else	return 0;
-	return 0;
-	return 0;
 }
 
 int test8()
 {
-	char * hungary_test = "aaa_bb_ccc_ddd";
+	char * downline_test = "aaa_bb_ccc_ddd";
 	char * anticipated_test = "aaa_bbCccDdd";
-	printf("Input is %s\n", hungary_test);
-	char * result = (char *)malloc(strlen(hungary_test) * 2);
-	memset(result, 0, strlen(hungary_test) * 2);
+	printf("Input is %s\n", downline_test);
+	char * result = (char *)malloc(strlen(downline_test) * 2);
+	memset(result, 0, strlen(downline_test) * 2);
 
-	DownLine2HungaryX(hungary_test, &result);
+	DownLine2HungaryX(downline_test, &result);
 
 	printf("result is %s \n", result);
 	if(strcmp(anticipated_test, result) == 0){
@@ -138,8 +135,133 @@ int test8()
 		return 1;
 	}
 	else	return 0;
-	return 0;
 }
+
+int test9()
+{
+	char * downline_test = "aaa_bb_ccc_ddd";
+	char * anticipated_test = "aaaBbCccDdd";
+	printf("Input is %s\n", downline_test);
+	char * result = DownLine2Camelback(downline_test);
+	printf("result is %s \n", result);
+	if(strcmp(anticipated_test, result) == 0){
+		// 缺陷： 函数内部回开辟内存空间  必须在函数外部进行释放
+		free(result);
+		return 1;
+	}	
+	else	return 0;
+}
+
+int test10()
+{
+	char * downline_test = "aaa_bb_ccc_ddd";
+	char * anticipated_test = "aaaBbCccDdd";
+	printf("Input is %s\n", downline_test);
+	char * result = (char *)malloc(strlen(downline_test) * 2);
+	memset(result, 0, strlen(downline_test) * 2);
+
+	DownLine2CamelbackX(downline_test, &result);
+
+	printf("result is %s \n", result);
+	if(strcmp(anticipated_test, result) == 0){
+		free(result);
+		return 1;
+	}
+	else	return 0;
+}
+
+int test11()
+{
+	char * downline_test = "aaa_bb_ccc_ddd";
+	char * anticipated_test = "AaaBbCccDdd";
+	printf("Input is %s\n", downline_test);
+	char * result = DownLine2Pascal(downline_test);
+	printf("result is %s \n", result);
+	if(strcmp(anticipated_test, result) == 0){
+		// 缺陷： 函数内部回开辟内存空间  必须在函数外部进行释放
+		free(result);
+		return 1;
+	}	
+	else	return 0;
+}
+
+int test12()
+{
+	char * downline_test = "aaa_bb_ccc_ddd";
+	char * anticipated_test = "AaaBbCccDdd";
+	printf("Input is %s\n", downline_test);
+	char * result = (char *)malloc(strlen(downline_test) * 2);
+	memset(result, 0, strlen(downline_test) * 2);
+
+	DownLine2PascalX(downline_test, &result);
+
+	printf("result is %s \n", result);
+	if(strcmp(anticipated_test, result) == 0){
+		free(result);
+		return 1;
+	}
+	else	return 0;
+}
+
+// int test13()
+// {
+
+// }
+
+// int test14()
+// {
+
+// }
+
+// int test15()
+// {
+
+// }
+
+// int test16()
+// {
+
+// }
+
+// int test17()
+// {
+
+// }
+
+// int test18()
+// {
+
+// }
+
+// int test19()
+// {
+
+// }
+
+// int test20()
+// {
+
+// }
+
+// int test21()
+// {
+
+// }
+
+// int test22()
+// {
+
+// }
+
+// int test23()
+// {
+
+// }
+
+// int test24()
+// {
+	
+// }
 
 int main()
 {
@@ -151,9 +273,21 @@ int main()
 	if(test6()) printf("test6 successed\n********************\n\n");
 	if(test7()) printf("test7 successed\n********************\n\n");
 	if(test8()) printf("test8 successed\n********************\n\n");
-	// if(test9()) printf("test9 successed\n********************\n\n");
-	// if(test10()) printf("test10 successed\n********************\n\n");
-	// if(test11()) printf("test11 successed\n********************\n\n");
-	// if(test12()) printf("test12 successed\n********************\n\n");
+	if(test9()) printf("test9 successed\n********************\n\n");
+	if(test10()) printf("test10 successed\n********************\n\n");
+	if(test11()) printf("test11 successed\n********************\n\n");
+	if(test12()) printf("test12 successed\n********************\n\n");
+	if(test13()) printf("test13 successed\n********************\n\n");
+	if(test14()) printf("test14 successed\n********************\n\n");
+	if(test15()) printf("test15 successed\n********************\n\n");
+	if(test16()) printf("test16 successed\n********************\n\n");
+	if(test17()) printf("test17 successed\n********************\n\n");
+	if(test18()) printf("test18 successed\n********************\n\n");
+	if(test19()) printf("test19 successed\n********************\n\n");
+	if(test20()) printf("test20 successed\n********************\n\n");
+	if(test21()) printf("test21 successed\n********************\n\n");
+	if(test22()) printf("test22 successed\n********************\n\n");
+	if(test23()) printf("test23 successed\n********************\n\n");
+	if(test24()) printf("test24 successed\n********************\n\n");
 	return 0;
 }
