d2c8625821f2764c95bab8889464925902ec0c93
M.K <m.k@MKdeMacBook-Pro.local>
2017-04-25 23:04:44 +0800

    sjp

--- Sort/SortBags.h
+++ Sort/SortBags.h
@@ -10,4 +10,12 @@ int PrintArray(int *arr, int length);
 //插入排序 升序
 int InsertSort(int *arr, int length);
 
+//合并逻辑
+int Merge(int arr[], int first, int mid, int last, int temp[]);
+
+//归并排序 升序
+int MergeSort(int arr[], int first, int last, int temp[]);
+
+
+
 #endif
--- Sort/test.c
+++ Sort/test.c
@@ -1,10 +1,39 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "SortBags.h"
+
+int test1(int arr[], int length)
+{
+    InsertSort(arr, length);
+    return 0;
+}
+
+
+int test2(int arr[], int length)
+{
+    for(int i = 0; i < length; i ++)
+    {
+        printf("%4d", arr[i]);
+    }
+    printf("\n");
+    int *temp = (int *)malloc(sizeof(int) * length);
+    memset(temp, 0, length);
+    MergeSort(arr, 0, length - 1, temp);
+    for(int i = 0; i < length; i ++)
+    {
+        printf("%4d", arr[i]);
+    }
+    printf("\n");
+    return 0;
+}
 
 int main(void)
 {
     int arr[] = {2,3,43,1,15,45,6,53,45,67,83,36,56,34};
-    InsertSort(arr, sizeof(arr) / sizeof(int));
+    //test1(arr, sizeof(arr) / sizeof(int));
+    //printf("*****************************\n");
+    test2(arr, sizeof(arr) / sizeof(int));
     return 0;
 }
 
